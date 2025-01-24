0b2c78d2cf6b9d145069337317503d160d5d0f8b
Yasir Munir <yasir.official128@gmail.com>
2025-01-24 05:11:25 +0500

    Create formValidation.js

---
+++ utils/formValidation.js
@@ -0,0 +1,5 @@
+// Utility function to validate email format using regex
+export const validateEmail = (email) => {
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return regex.test(email);
+};
